8d4f8540ce153a94995a635e35fe27f106d60a44
Jared Lewis <jareddlewis@gmail.com>
2026-06-30 14:02:02 -0600

    fix matching empty string to empty string bug. randomize answer

--- mastermind.js
+++ mastermind.js
@@ -6,12 +6,20 @@ const prompt = require("prompt-sync")();
 // black: right color, right spot
 
 // Red(R), Green(G), Blue(B), Yellow(Y), Cyan(C), Purple(P)
+const colors = ['R', 'G', 'B', 'Y', 'C', 'P'];
 
+const answer = [];
+
+for (let i = 0; i < 4; i++) {
+  answer[i] = colors[Math.floor(Math.random() * 6)]
+}
+
+console.log(answer)
 // What is your guess?(format: XXXX)GGBG
 // 1 right color right place
 // 3 right color wrong place
 
-const answer = ["R", "C", "G", "G"];
+// const answer = ["R", "C", "G", "G"];
 
 function checkForValidAnswers(userGuess, answer) {
   let whiteDot = 0;
@@ -29,8 +37,10 @@ function checkForValidAnswers(userGuess, answer) {
   });
 
   userGuessArr.forEach((value, index) => {
-    if (answer.includes(value)) {
-      console.log(`White at index ${ index }: ${answer}, ${userGuess}`);
+    if (answer.includes(value) && value) {
+
+      console.log(`White at index ${ index }: ${answer}, ${userGuess}; value ${value}`);
+      userGuessArr[index] = "";
       answer[answer.indexOf(value)] = "";
       whiteDot++;
     }
@@ -45,4 +55,8 @@ for (let i = 0; i < guessesAllowed; i++) {
   const userGuess = prompt("What is your guess? (format: XXXX) ");
   const checkResult = checkForValidAnswers(userGuess, [...answer]);
   console.log(`${checkResult.blackDot} right color right place\n${checkResult.whiteDot} right color wrong place\n`);
+  if (checkResult.blackDot === 4) {
+    console.log('Congratulations! You Win!')
+    break;
+  }
 }
